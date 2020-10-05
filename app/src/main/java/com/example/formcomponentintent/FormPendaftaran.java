4b536c245e48f321de221c4f1dcf1ad01462df59
nabilahaffryda <sehungantengs@gmail.com>
2020-10-05 22:39:09 +0700

    layout page 1

---
+++ app/src/main/java/com/example/formcomponentintent/FormPendaftaran.java
@@ -0,0 +1,14 @@
+package com.example.formcomponentintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormPendaftaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_pendaftaran);
+    }
+}
